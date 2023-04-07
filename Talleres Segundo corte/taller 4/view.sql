2b93fd384af47214c75cafc4d93c138688e18f85
Blackjac12 <124262044+Blackjac12@users.noreply.github.com>
2023-04-07 12:38:07 -0500

    Update view.sql

--- Talleres Segundo corte/taller 4/view.sql
+++ Talleres Segundo corte/taller 4/view.sql
@@ -26,21 +26,50 @@ AND  b.id_aprovados = a.id
 AND b.becados = 1;
 
 
-CREATE OR REPLACE
-ALGORITHM = UNDEFINED VIEW `Admicion`.`materias` AS
-select
-    `m`.`nombre` AS `nombre`,
-    `m`.`creditos` AS `creditos`,
-    `c`.`carrera` AS `carrera`
-from
-    (`Admicion`.`carrera` `c`
-join `Admicion`.`materia` `m`)
-where
-    `m`.`id_carrera` = `c`.`id`;
+CREATE  view materias as
+SELECT m.nombre,m.creditos  ,c.carrera 
+FROM  materia m , carrera c 
+WHERE m.id_carrera = c.id 
     
 CREATE  view odern As 
 Select *
 FROM  contacto c  
 ORDER BY c.nombre  ASC
     
+    
+CREATE  view Orden_materias as
+SELECT m.nombre,m.creditos  ,c.carrera 
+FROM  materia m , carrera c 
+WHERE m.id_carrera = c.id 
+ORDER BY m.nombre  ASC 
+    
+    
+CREATE VIEW aprovados_ordanasos  As  
+SELECT c2.nombre ,c2.apellido ,c.carrera ,r.sede ,a.aprovados 
+FROM registro r ,carrera c ,contacto c2 ,Aprovados a 
+WHERE r.id_concto  = c2.id 
+and r.id_carrera = c.id 
+AND a.id_registrados = r.id 
+and a.aprovados = 1
+ORDER BY c2.nombre ASC  
+
+
+CREATE  view pines_pagos as
+Select p.perido , p.año , p.valor_pagar 
+FROM pin p 
+ORDER BY p.valor_pagar  DESC
+
+
+
+CREATE  view   peor_puntaje as
+SELECT c2.nombre ,c2.apellido ,c.carrera , i.puntaje as puntaje_de_ifeces ,r.sede 
+FROM registro r ,carrera c ,ifeces i,contacto c2 
+WHERE r.id_concto  = c2.id 
+and r.id_carrera = c.id 
+and r.id_ifeces = i.id
+ORDER BY i.puntaje  ASC
+ 
+ 
+ 
+ 
     
