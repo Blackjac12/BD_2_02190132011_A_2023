d15b84fb88833276854f8be58061ad238df15361
Blackjac12 <124262044+Blackjac12@users.noreply.github.com>
2023-04-09 10:49:43 -0500

    Update view.sql

--- Talleres Segundo corte/taller 4/view.sql
+++ Talleres Segundo corte/taller 4/view.sql
@@ -85,4 +85,65 @@ and r.id_carrera = c.id
 and r.id_ifeces = i.id
 ORDER BY i.puntaje  DESC
 
- 
+
+CREATE VIEW aprovados_desdiente  As  
+SELECT c2.nombre ,c2.apellido ,c.carrera ,r.sede ,a.aprovados 
+FROM registro r ,carrera c ,contacto c2 ,Aprovados a 
+WHERE r.id_concto  = c2.id 
+and r.id_carrera = c.id 
+AND a.id_registrados = r.id 
+and a.aprovados = 1
+ORDER BY c2.nombre DESC 
+
+
+CREATE  view Orden_creditos as
+SELECT m.nombre,m.creditos  ,c.carrera 
+FROM  materia m , carrera c 
+WHERE m.id_carrera = c.id 
+ORDER BY m.creditos  DESC 
+
+
+
+ CREATE  view Orden_creditos2 as
+SELECT m.nombre,m.creditos  ,c.carrera 
+FROM  materia m , carrera c 
+WHERE m.id_carrera = c.id 
+ORDER BY m.creditos   ASC
+
+
+
+CREATE  view horario as
+SELECT c2.nombre ,c2.apellido ,c.carrera ,r.sede ,m.creditos  
+FROM registro r ,carrera c ,contacto c2 ,Aprovados a,materia m  
+WHERE r.id_concto  = c2.id 
+and r.id_carrera = c.id 
+AND a.id_registrados = r.id
+and m.id_carrera = c.id 
+and a.aprovados = 1
+
+CREATE VIEW becados_orden  As 
+Select c.nombre ,c.apellido ,r.sede ,c2.carrera, b.becados
+FROM  contacto c , carrera c2 ,registro r ,Aprovados a ,beca b 
+WHERE r.id_concto  = c.id 
+and r.id_carrera =c2.id 
+AND a.id_registrados = r.id 
+AND  a.id_registrados =r.id 
+AND  b.id_aprovados = a.id 
+AND b.becados = 1
+ORDER BY c.nombre ASC 
+
+
+CREATE  view carerar_desorden  as
+Select *
+FROM  carrera c 
+ORDER BY c.carrera ASC 
+
+
+
+CREATE  view carerar_desorden  as
+Select *
+FROM  carrera c 
+ORDER BY c.carrera DESC  
+
+
+
